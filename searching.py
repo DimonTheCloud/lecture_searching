15281f2c08616ced6866a364e5d1a87dfb8ecb15
Dmytro Antonenko <dimchik41414@gmail.com>
2026-04-13 13:29:33 +0200

    linear_search

--- searching.py
+++ searching.py
@@ -1,4 +1,3 @@
-from pathlib import Path
 import json
 
 
@@ -7,16 +6,27 @@ def read_data(file_name, field):
         data = json.load(json_file)
 
     allowed_fields = {"unordered_numbers", "ordered_numbers", "dna_sequence"}
-
     if field not in allowed_fields:
         return None
 
     return data[field]
 
 
+def linear_search(sequence, target):
+    positions = []
+
+    for index, value in enumerate(sequence):
+        if value == target:
+            positions.append(index)
+    return {"positions": positions, "count": len(positions)}
+
+
 def main():
     sequential_data = read_data("sequential.json", "unordered_numbers")
     print(sequential_data)
+    target = 0
+    result = linear_search(sequential_data, target)
+    print(result)
 
 
 if __name__ == "__main__":
